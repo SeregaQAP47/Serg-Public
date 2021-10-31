0b3c21964f1f6b44bf2429360144e21c8d4381d7
unknown <C:\Users\yagun\AppData\Roaming\The Bat!>
2021-10-31 22:29:19 +0300

    Задание 16.7.1 Наследование класса

---
+++ CatTest.py
@@ -0,0 +1,12 @@
+from CatAnimal import Cat
+
+c1 = Cat("Барон", "Мальчик", 2)
+c2 = Cat("Сэм", "Мальчик", 2)
+
+print("c1.name ", c1.name)
+print("c1.gender ", c1.gender)
+print("c1.age ", str(c1.age))
+print()
+print("c2.name ", c2.name)
+print("c2.gender ", c2.gender)
+print("c2.age ", str(c2.age))
